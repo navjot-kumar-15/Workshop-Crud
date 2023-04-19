45f180ee8bdeb12f5ee593b1efb26e23d723b1c2
navjot-kumar-15 <110272866+navjot-kumar-15@users.noreply.github.com>
2023-04-19 12:35:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/", Routes);
 Connection();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT} `);
 });
